e3c72123fc5c4b6c2cfa7280b5c130b61dd2c41e
Vidic28 <vinicioguillena@gmail.com>
2025-04-02 01:05:14 -0600

    editar producto full version

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -54,14 +55,16 @@ class ProductoController extends Controller
 
     public function actualizar_producto(Request $request, $id)
     {
-        $this->validateProduct($request);
-        $productos = EsquemaProducto::findOrFail($id);
-        $codigoExistente = EsquemaProducto::where('codigo_producto', $request->codigo_product)->first();
-
-        if ($codigoExistente) {
-            return redirect()->route('producto')->with('error', 'El codigo ya está existe.');
-        }
-
+        $this->validateProduct($request, [
+            'codigo_product' => [
+                'required', 
+                'string', 
+                'max:255', 
+                Rule::unique('esquema_producto')->ignore($id, 'id_esquema_producto') // Ignora el producto actual
+            ],
+        ]);
+    
+        // Procedimiento almacenado para actualizar el producto
         DB::statement('EXEC sp_actualizar_esquema_producto
             @id_esquema_producto = ?, 
             @codigo_producto = ?, 
@@ -74,10 +77,12 @@ class ProductoController extends Controller
                 $request->descripcion_product
             ]
         );
-
-        // Redirigir con mensaje de éxito
+    
+        // Redirigir con éxito
         return redirect()->route('producto')->with('success', 'Producto actualizado exitosamente.');
+        
     }
+
 
     public function cambiar_estado($id)
     {
