eac1f9ed488bbbab4d9882f4549f258a4228d599
Vidic28 <vinicioguillena@gmail.com>
2025-04-02 21:40:35 -0600

    buscador producto

--- resources/views/compras/producto.blade.php
+++ resources/views/compras/producto.blade.php
@@ -1,5 +1,15 @@
 <x-admin-layout>
     <b><h1 class="text-lg text-center dark:text-black">PRODUCTOS</h1></b>
+
+        <form method="GET" action="{{ route('producto') }}" class="mb-4">
+            <div class="flex space-x-2">
+                <input type="text" name="buscador" placeholder="Buscar por código o nombre" 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <button type="submit" class="bg-blue-700 text-white px-4 py-2 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Buscar
+                </button>
+            </div>
+        </form>
         <!-- Modal toggle -->
         <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
             Agregar Producto
@@ -143,5 +153,4 @@
         </tbody>
     </table>
 </div>
-
 </x-admin-layout>
