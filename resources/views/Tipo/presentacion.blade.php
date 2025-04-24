e57e801298f152ca16ba9b40ca35c96bc9ae0c08
Kath-Bail <athaby.120@gmail.com>
2025-04-24 13:15:47 -0600

    Tipos

--- resources/views/Tipo/presentacion.blade.php
+++ resources/views/Tipo/presentacion.blade.php
@@ -139,7 +139,7 @@
                                         <div class="col-span-2">
                                             <label for="presentacion"
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del tipo de Presentacion</label>
-                                            <input type="text" id="presentacion" name="nombre_tpresentacionipo_venta"
+                                            <input type="text" id="presentacion" name="presentacion"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                                 required
                                                 value="{{ old('presentacion', $presentacion->presentacion) }}">
