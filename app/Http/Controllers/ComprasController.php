8ee84f2c417dca434216ebbabc5610006c98efba
Vidic28 <vinicioguillena@gmail.com>
2025-05-12 20:01:25 -0600

    capa 8

--- app/Http/Controllers/ComprasController.php
+++ app/Http/Controllers/ComprasController.php
@@ -43,29 +43,21 @@ class ComprasController extends Controller
 
     public function crearCompra(Request $request)
     {
-        // Recoger los productos y otros datos de la solicitud
-        $productos = $request->input('productos');  // Un array de productos
-        // Crear una tabla temporal de productos en SQL Server
+
+        $productos = $request->input('productos');  
         $productosTable = [];
         foreach ($productos as $producto) {
-            // Convertir las fechas con Carbon y asegurarse de que estén en el formato correcto
             $fechaFabricacion = Carbon::parse($producto['fecha_fabricacion'])->format('Ymd');
             $fechaVencimiento = Carbon::parse($producto['fecha_vencimiento'])->format('Ymd');           
-
-            // Log para verificar las fechas
-            \Log::info("Producto ID: {$producto['id_esquema_producto']} - Fecha Fabricación: {$fechaFabricacion} - Fecha Vencimiento: {$fechaVencimiento}");
     
-            // Validar que las fechas no sean nulas o vacías
             if (empty($fechaFabricacion) || empty($fechaVencimiento)) {
                 return response()->json(['error' => 'Las fechas de fabricación y vencimiento no pueden estar vacías.'], 400);
             }
-    
-            // Asegurarse de que las fechas estén dentro del rango válido de SQL Server
+
             if (!$this->isValidDate($fechaFabricacion) || !$this->isValidDate($fechaVencimiento)) {
                 return response()->json(['error' => 'Fecha fuera de rango o formato inválido.'], 400);
             }
-    
-            // Agregar el producto a la tabla temporal
+
             $productosTable[] = [
                 'IdEsquemaProducto' => $producto['id_esquema_producto'],
                 'Lote' => $producto['lote'],
@@ -79,7 +71,6 @@ class ComprasController extends Controller
             ];
         }
     
-        // Log para verificar la consulta generada
         $insertValues = [];
         foreach ($productosTable as $producto) {
             $insertValues[] = sprintf(
@@ -97,8 +88,6 @@ class ComprasController extends Controller
             
         }
         $insertQuery = implode(', ', $insertValues);
-        \Log::info("Insert Query: " . $insertQuery);
-        // Obtener la empresa del usuario actual
         $idEmpresa = Auth::user()->empresa()->where('estado', 'A')->first()?->id_empresa;
 
         if (!$idEmpresa) {
@@ -112,7 +101,6 @@ class ComprasController extends Controller
             return back()->with('error', 'La fecha de compra no puede ser mayor a la fecha actual.');
         }
 
-        // Ejecutar el procedimiento almacenado usando una tabla temporal
         DB::transaction(function () use ($request, $insertQuery, $idEmpresa) {
             DB::statement("
             DECLARE @Productos TipoProductos;
@@ -135,8 +123,7 @@ class ComprasController extends Controller
 
         });
         
-    
-        // Redirigir con un mensaje de éxito
+
         return redirect()->route('compras')->with('success', 'Compra registrada exitosamente.');
     }
 
@@ -148,12 +135,10 @@ class ComprasController extends Controller
     }
     
 
-    // Función para verificar si la fecha está en un rango válido para SQL Server
     private function isValidDate($date)
     {
         try {
             $parsedDate = Carbon::parse($date);
-            // Verificar si la fecha está en el rango válido de SQL Server
             return $parsedDate->between(Carbon::createFromDate(1753, 1, 1), Carbon::createFromDate(9999, 12, 31));
         } catch (\Exception $e) {
             return false;
