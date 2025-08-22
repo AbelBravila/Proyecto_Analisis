e273c021795abf99cf267517546f944aacc1d868
DarwinEs <estudiodarwine3@gmail.com>
2025-08-21 18:36:33 -0600

    Credenciales Firebase

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -13,10 +13,20 @@ class FirebaseService
 
     public function __construct()
     {
-        $factory = (new Factory)->withServiceAccount(
-            storage_path('app\firebase\posmovil-37d5e-firebase-adminsdk-fbsvc-595e34fa9b.json')
-        );
+        $firebaseConfig = [
+            'type' => 'service_account',
+            'project_id' => env('FIREBASE_PROJECT_ID'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID', ''), 
+            'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => env('FIREBASE_CLIENT_ID', ''),
+            'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri' => 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url' => env('FIREBASE_CLIENT_CERT_URL', ''),
+        ];
 
+        $factory = (new Factory)->withServiceAccount($firebaseConfig);
         $this->messaging = $factory->createMessaging();
     }
 
