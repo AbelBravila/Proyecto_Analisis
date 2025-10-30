fd74719c8ee11321b49c48e6ae6dee5fef5db215
DarwinEs <estudiodarwine3@gmail.com>
2025-10-29 22:44:26 -0600

    Correo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('Correo') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="correo" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
